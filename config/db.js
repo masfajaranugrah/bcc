47f9afe937b7016f2e3565ca98e82824efb07f1a
masfajaranugrah <fajaranugrah2203@gmail.com>
2025-07-04 08:06:33 +0700

    new

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
-import {Sequelize} from 'sequelize';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-const envFile = process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.prod';
 
-dotenv.config({path: envFile})
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    
-    {
-  host:  process.env.DB_HOST,
+const envFile = process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.prod';
+dotenv.config({ path: envFile });
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,  
+    },
+  },
 });
 
- export default sequelize;
+export default sequelize;
